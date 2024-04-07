d98644361e96f9c0d30650871508a9e3f6e0bd67
Muhammed Tijani <coolzyte@gmail.com>
2024-04-07 15:49:28 +0100

    added the search component and logic

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -2,7 +2,9 @@ import mongoose from 'mongoose'
 
 async function dbConnect() {
   try {
-    await mongoose.connect(process.env.MONGODB_URI!)
+    await mongoose.connect(process.env.MONGODB_URI!, {
+      maxPoolSize: 50, // Increase the maximum number of connections in the pool
+    })
   } catch (error) {
     throw new Error('Connection failed!')
   }
