98fc9b2100f9104fd669cff9ffb18f19fe6f3052
Muhammed Tijani <coolzyte@gmail.com>
2024-04-07 16:10:03 +0100

    modified the products categories

--- app/api/products/categories/route.ts
+++ app/api/products/categories/route.ts
@@ -1,39 +1,8 @@
-// import dbConnect from '@/lib/dbConnect'
-// import ProductModel from '@/lib/models/ProductModel'
-
-// export const GET = async (req: any) => {
-//   await dbConnect()
-
-//   // Ensure the 'category' field has an index for faster distinct retrieval
-//   try {
-//     const categories = await ProductModel.find().distinct('category')
-//     return Response.json(categories)
-//   } catch (error) {
-//     console.error(error) // Log the error for debugging
-//     return Response.json(
-//       { error: 'Failed to retrieve categories' },
-//       { status: 500 }
-//     ) // Send error response
-//   }
-// }
-
 import dbConnect from '@/lib/dbConnect'
 import ProductModel from '@/lib/models/ProductModel'
 
 export const GET = async (req: any) => {
   await dbConnect()
-
-  try {
-    const categories = await ProductModel.aggregate([
-      { $group: { _id: '$category' } },
-      { $project: { _id: 0, category: '$_id' } },
-    ])
-    return Response.json(categories.map((doc) => doc.category))
-  } catch (error) {
-    console.error('Error retrieving categories:', error)
-    return Response.json(
-      { error: 'Failed to retrieve categories' },
-      { status: 500 }
-    )
-  }
+  const categories = await ProductModel.find().distinct('category')
+  return Response.json(categories)
 }
--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -2,9 +2,7 @@ import mongoose from 'mongoose'
 
 async function dbConnect() {
   try {
-    await mongoose.connect(process.env.MONGODB_URI!, {
-      maxPoolSize: 50, // Increase the maximum number of connections in the pool
-    })
+    await mongoose.connect(process.env.MONGODB_URI!)
   } catch (error) {
     throw new Error('Connection failed!')
   }
