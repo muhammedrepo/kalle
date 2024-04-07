0531337e494c9acd5538bf8762e7a865ba9385ea
Muhammed Tijani <coolzyte@gmail.com>
2024-04-07 14:50:24 +0100

    modified the dbConnect function

--- app/api/products/categories/route.ts
+++ app/api/products/categories/route.ts
@@ -3,6 +3,16 @@ import ProductModel from '@/lib/models/ProductModel'
 
 export const GET = async (req: any) => {
   await dbConnect()
-  const categories = await ProductModel.find().distinct('category')
-  return Response.json(categories)
+
+  // Ensure the 'category' field has an index for faster distinct retrieval
+  try {
+    const categories = await ProductModel.find().distinct('category')
+    return Response.json(categories)
+  } catch (error) {
+    console.error(error) // Log the error for debugging
+    return Response.json(
+      { error: 'Failed to retrieve categories' },
+      { status: 500 }
+    ) // Send error response
+  }
 }
--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -1,19 +1,10 @@
 import mongoose from 'mongoose'
 
-/**
- * dbConnect Function
- * This asynchronous function connects to a MongoDB database using the connection string provided in the environment variable MONGODB_URI.
- * If the connection fails, it throws an error.
- */
 async function dbConnect() {
   try {
-    // Attempt to establish a connection to the MongoDB database
-    await mongoose.connect(process.env.MONGODB_URI!, {
-      serverSelectionTimeoutMS: 60000,
-    }) // 60 seconds
+    await mongoose.connect(process.env.MONGODB_URI!)
   } catch (error) {
-    // If the connection fails, throw an error
-    throw new Error('Error connecting to database')
+    throw new Error('Connection failed!')
   }
 }
 
